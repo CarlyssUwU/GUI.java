2d513f1db1b935f1e8faee427e90a05e25996c09
CarlyssUwU <128750114+CarlyssUwU@users.noreply.github.com>
2023-04-04 14:24:58 +0200

    Update Hub.java

--- Hub.java
+++ Hub.java
@@ -3,8 +3,8 @@ public class Hub {
     Container[][] containers = new Container[10][12];
 
     public void stackContainers(int priority) {
-        for (int i = 0; i < 10; i++) {
-            Container container = new Container();
+        for (int i = 9; i >=0; i--) {
+            Container container = new Container(priority);
             if (priority == 1 && containers[i][0] == null) {
                 containers[i][0] = container;
                 break;
@@ -12,7 +12,7 @@ public class Hub {
                 containers[i][1] = container;
                 break;
             } else if (priority > 2) {
-                for (int j = 2; j < 12; j++) {
+                for(int j=2;j<12;j++){
                     if (containers[i][j] == null) {
                         containers[i][j] = container;
                         break;
@@ -21,14 +21,13 @@ public class Hub {
             }
         }
     }
-
     public String toString() {
         String aux = "";
         for (int i = 0; i < 10; i++) {
             aux += "\n";
             for (int j = 0; j < 12; j++) {
                 if (containers[i][j] != null) {
-                    aux += containers[i][j].getIdentifier();
+                    aux += "F";
                 } else {
                     aux += "0";
                 }
@@ -39,8 +38,8 @@ public class Hub {
     }
 
     public boolean isFulled() {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 12; j++) {
+        for (int i = 10; i >=0; i--) {
+            for (int j = 12; j >=0; j--) {
                 if (containers[i][j] == null) {
                     return false;
                 }

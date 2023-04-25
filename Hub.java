5eed4dffb43f6a5a7ec75006c7bd6f33a207294a
CarlyssUwU <128750114+CarlyssUwU@users.noreply.github.com>
2023-04-25 18:52:38 +0200

    Hub.java

--- Hub.java
+++ Hub.java
@@ -2,32 +2,49 @@ package port;
 public class Hub {
     Container[][] containers = new Container[10][12];
 
-    public void stackContainers(int priority) {
-        for (int i = 9; i >=0; i--) {
-            Container container = new Container(priority);
-            if (priority == 1 && containers[i][0] == null) {
-                containers[i][0] = container;
-                break;
-            } else if (priority == 2 && containers[i][1] == null) {
-                containers[i][1] = container;
-                break;
-            } else if (priority > 2) {
-                for(int j=2;j<12;j++){
-                    if (containers[i][j] == null) {
-                        containers[i][j] = container;
-                        break;
+    public void stackContainers(Container container) {
+        if (container != null) {
+            int priority = container.getPriority();
+            if (priority == 1 || priority == 2) {
+                int column = priority - 1;
+                for (int i = containers.length - 1; i >= 0; i--) {
+                    if (containers[i][column] == null) {
+                        containers[i][column] = container;
+                        return;
+                    }
+                }
+            } else {
+                for (int j = 2; j < containers[0].length; j++) {
+                    for (int i = containers.length - 1; i >= 0; i--) {
+                        if (containers[i][j] == null) {
+                            containers[i][j] = container;
+                            return;
+                        }
                     }
                 }
             }
         }
     }
-    public String toString() {
+
+    public Container unstackContainers(int col) {
+        if (col < 0 || col >= containers[0].length) return null;
+        for (int i = 0; i < containers.length; i++) {
+            if (containers[i][col] != null) {
+                Container aux = containers[i][col];
+                containers[i][col] = null;
+                return aux;
+            }
+        }
+        return null;
+    }
+
+    public String showHub() {
         String aux = "";
         for (int i = 0; i < 10; i++) {
             aux += "\n";
             for (int j = 0; j < 12; j++) {
                 if (containers[i][j] != null) {
-                    aux += "F";
+                    aux += containers[i][j].getIdentifier();
                 } else {
                     aux += "0";
                 }
@@ -37,14 +54,26 @@ public class Hub {
         return aux;
     }
 
-    public boolean isFulled() {
-        for (int i = 10; i >=0; i--) {
-            for (int j = 12; j >=0; j--) {
-                if (containers[i][j] == null) {
-                    return false;
+    public boolean isFulled(Container container) {
+        switch (container.getPriority()) {
+            case 1:
+                if (containers[0][0] != null) {
+                    return true;
                 }
-            }
+                break;
+            case 2:
+                if (containers[0][1] != null) {
+                    return true;
+                }
+                break;
+            case 3:
+                if (containers[0][11] != null) {
+                    return true;
+                }
+                break;
+            default:
+                return false;
         }
-        return true;
+        return false;
     }
 }
